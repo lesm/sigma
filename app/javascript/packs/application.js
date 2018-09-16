8cd10eb2cc01d90399b8ddc08a31de6ae78dcb81
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2018-09-15 22:23:37 -0500

    Fixing ContentWrapperHeight problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,9 @@ import '../src/js/notify_alert_confirm'
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+
+function resizeContentWrapperHeight(){
+  $(document).trigger("resize");
+}
+
+$(document).on("turbolinks:load", resizeContentWrapperHeight)
