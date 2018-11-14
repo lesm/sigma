1e300b30f2f35f653939f464951e30b17ce0b85d
iTox <juanthoniovg@gmail.com>
2018-11-14 08:56:54 -0600

    Add some tests
    Enable validation html5
    Concepto's importe must be cantidad by valor_unitario

--- app/models/concepto.rb
+++ app/models/concepto.rb
@@ -3,7 +3,9 @@ class Concepto < ApplicationRecord
   has_one :datos_concepto, dependent: :destroy
   accepts_nested_attributes_for :datos_concepto, allow_destroy: true
 
-  validates :importe, presence: true
+  validates :importe, :cantidad, :valor_unitario, presence: true
+  validates :cantidad, numericality: { only_integer: true }
+  validate :importe_value
 
   delegate :folio, :clave_catastral, :numero_cuenta, :ubicacion,
     :base_catastral, :impuesto_predial, :observaciones, :fecha,
@@ -13,4 +15,12 @@ class Concepto < ApplicationRecord
     :numero_contrato, :numero_medidor, :lectura_actual,
     :lectura_anterior, :consumo, :ruta, :lecturista,
     to: :datos_concepto, prefix: false, allow_nil: true
+
+  def importe_value
+    return if cantidad.nil? or valor_unitario.nil?
+    resultado = cantidad * valor_unitario
+    if importe != resultado
+      errors.add :importe, "debe ser #{resultado}"
+    end
+  end
 end
