21a1e4b84b21cb046bc4bd08ca4ec4d7964161bc
iTox <juanthoniovg@gmail.com>
2018-11-14 13:48:05 -0600

    Add validation to subtotal and total of comprobante

--- spec/models/comprobante_spec.rb
+++ spec/models/comprobante_spec.rb
@@ -21,4 +21,26 @@ RSpec.describe Comprobante, type: :model do
   it { should validate_presence_of :total }
   it { should validate_numericality_of(:total).is_greater_than(0) }
   it { should validate_numericality_of(:subtotal).is_greater_than(0) }
+
+  describe "#valida_subtotal" do
+    let(:concepto) { build :concepto, cantidad: 2, valor_unitario: 200, importe: 400 }
+    let(:comprobante) { build :comprobante, conceptos: [concepto], subtotal: 500, total: 400 }
+    context "#subtotal" do
+      it "must be equals to total" do
+        expect(comprobante).to be_invalid
+        expect(comprobante.errors.to_a.to_sentence).to match /debe ser igual a total/
+      end
+    end
+  end
+
+  describe "#valida_total" do
+    let(:concepto) { build :concepto, cantidad: 2, valor_unitario: 200, importe: 400 }
+    let(:comprobante) { build :comprobante, conceptos: [concepto], subtotal: 400, total: 500 }
+    context "#total" do
+      it "must be equals to sum of conceptos' importe" do
+        expect(comprobante).to be_invalid
+        expect(comprobante.errors.to_a.to_sentence).to match /debe ser igual a la suma del importe de los conceptos/
+      end
+    end
+  end
 end
