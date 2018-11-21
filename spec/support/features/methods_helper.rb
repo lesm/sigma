5a8fef2f86cfc7046e9a8f5b9bf464b97cb38140
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2018-11-21 14:36:26 -0600

    Add small refactor

--- spec/support/features/methods_helper.rb
+++ spec/support/features/methods_helper.rb
@@ -1,5 +1,14 @@
 module Features
   module MethodsHelpers
+    def dado_que_hay_un_contribuyente_una_cuenta_y_un_emisor
+      create :contribuyente, nombre_o_razon_social: "Carlos",
+        primer_apellido: "José",
+        segundo_apellido: "Pérez",
+        rfc: "AAAA111111AAZ"
+      create :cuenta, codigo: "110101", descripcion: "RIFAS", formato: "DatosComun"
+      create :emisor, :con_direccion
+    end
+
     def dado_que_hay_un_cajero_logueado_con_una_caja
       sign_in_cajero_with_caja
     end
@@ -12,8 +21,18 @@ module Features
       expect(page).to have_current_path(recibo_step_path(:set_cuenta))
     end
 
+    def cajero_selecciona_un_contribuyente_y_un_concepto_de_cobro
+      select "Carlos José Pérez - AAAA111111AAZ", from: "Contribuyente"
+      select "110101 - DatosComun - RIFAS", from: "cuenta_ids"
+    end
+
     def cuando_da_click_en_el_link_siguiente
       click_button "Siguiente"
     end
+
+    def cajero_es_redireccionado_a_set_conceptos_de_recibo_steps
+      expect(page).to have_current_path(/recibo_steps\/set_conceptos/)
+    end
+
   end
 end
