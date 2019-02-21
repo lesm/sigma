bf46b7a5e371744676ad0aa3585da8c0c8386bbc
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2019-02-20 19:57:43 -0600

    Changing method name

--- app/models/comprobante/crea_pdf.rb
+++ app/models/comprobante/crea_pdf.rb
@@ -13,13 +13,13 @@ class Comprobante
 
     def crea_pdf_file
       Tempfile.open("pdf_file", Rails.root.join("tmp")) do |f|
-        f.write(pdf_fm_encode)
+        f.write(pdf_encode)
         f.close
         f
       end
     end
 
-    def pdf_fm_encode
+    def pdf_encode
       GenerarPdf.new.generar(comprobante)
         .force_encoding('utf-8').encode
     end
